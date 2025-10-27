fad14d1cabcbfe65b44be15527ca4192f5896461
beniskossi <68175656+beniskossi@users.noreply.github.com>
2025-10-27 13:59:44 +0000

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,4 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-draw-predictions.ts';
--- src/app/api/loto-results/route.ts
+++
@@ -1,3 +0,0 @@
-
-// This file is intentionally left blank.
-// It has been deleted as it was deprecated and no longer in use.
